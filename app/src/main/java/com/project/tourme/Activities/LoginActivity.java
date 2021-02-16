42ff20fa448a6465f7554893c97d5641d5a5fcd4
FareeduddinAAP <fareed.cs1998@gmail.comgit config --global user.email fareed.cs1998@gmail.com>
2021-02-16 08:00:11 +0500

    Authentication Login and Registration

--- app/src/main/java/com/project/tourme/Activities/LoginActivity.java
+++ app/src/main/java/com/project/tourme/Activities/LoginActivity.java
@@ -1,18 +1,139 @@
 package com.project.tourme.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.tourme.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    TextView createNewAccount;
+    EditText inputUserEmail, inputPassword;
+    Button btnLogin;
+    ProgressDialog mLoadingBar;
+
+    ProgressDialog progressBar;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference mUserRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+
+        //variable init
+        inputUserEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        createNewAccount = findViewById(R.id.createNewAccount);
+
+
+//        inputPassword.setTransformationMethod(new PasswordTransformationMethod());
+
+        mLoadingBar = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mUserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LoginUser();
+            }
+        });
     }
+
+    private void LoginUser() {
+        String email = inputUserEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (email.isEmpty() || !email.contains("@")) {
+            inputUserEmail.setError("Enter Correct format Email");
+            inputUserEmail.requestFocus();
+        } else if (password.isEmpty() || password.length() < 6) {
+            inputPassword.setError("ENter min 7 latter password");
+        } else {
+            progressBar.setMessage("Login...");
+            progressBar.setCanceledOnTouchOutside(false);
+            progressBar.show();
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        mUser = mAuth.getCurrentUser();
+                        CHeckUserData();
+                        progressBar.dismiss();
+                    } else {
+                        progressBar.dismiss();
+                        Toast.makeText(LoginActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        }
+    }
+
+    //check Editer user saved Setup profile data or not.If not then send user t setup Page to add this data
+    private void CHeckUserData() {
+        mUserRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child("username").exists()) {
+                    //if user saved data of setup then open main page
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    //if user did not saved data of setup page then open setuppage
+                    Intent intent = new Intent(LoginActivity.this, SetupActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/project/tourme/Activities/SplashActivity.java
+++ app/src/main/java/com/project/tourme/Activities/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.project.tourme.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,13 +11,27 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.tourme.R;
 
 public class SplashActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView textView;
+
+    //Variable Init
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference mUserRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,22 +44,59 @@ public class SplashActivity extends AppCompatActivity {
         Animation animationlogo;
         Animation animationText;
 
-
         animationlogo= AnimationUtils.loadAnimation(this,R.anim.my_logo_anim);
         animationText= AnimationUtils.loadAnimation(this,R.anim.text_anim);
         imageView.setAnimation(animationlogo);
         textView.setAnimation(animationText);
 
+        //Firebase Decleration
+        mUserRef= FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        //wait for 5 sec and open main activity
+        Runnable runnable = () -> {
 
-        Runnable runnable=new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (mUser!=null)
+            {
+                CHeckUserData();
+            }else
+            {
+                //open new activity
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
                 finish();
             }
         };
-        new Handler().postDelayed(runnable,4000);
+        new Handler().postDelayed(runnable, 5000);
+    }
+    //check Edither  user saved setup data or not
+    private void CHeckUserData() {
+        mUserRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child("username").exists())
+                {
+                    //if user saved data of setup then open main page
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }else {
+
+                    //if user did not saved data of setup page then open setup page
+                    Intent intent = new Intent(SplashActivity.this, SetupActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(SplashActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
